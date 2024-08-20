456a35d4e728b485b44801dfcfdc353ad2fb13f2
alberto <alb.jauregui@gmail.com>
2024-08-20 08:10:27 -0600

    fundamentos unidad2 actividad3 inicio

---
+++ fundamentosDeProgramacion/unidad2/actividad3/main.c
@@ -0,0 +1,25 @@
+// biblioteca de esntrada y salida estandar
+#include <stdio.h>
+
+// funcion principal
+int main() {
+
+  // declaracion de variables
+  int opcion;
+
+  // impresion de datos del estudiante
+  printf("Bienvenido al sistema de gestion de inventario y ventas PyroTEC\n"
+         "***Luis Alberto Jauregui Escobar***\n"
+         "***ES231105647***\n"
+         "***DS-DFPR-2402-B1-018***\n\n");
+
+  // impresion de menu haha no mamse no
+  printf("Menu de opciones:\n"
+         "1. Mostrar inventario actualizado\n"
+         "2. Realizar venta\n"
+         "3. Agregar stock a un producto\n"
+         "4. Calcular total de ventas del dia\n"
+         "5. Salir\n\n"
+         "Ingrese su opcion: ");
+  scanf("%d", &opcion);
+}
